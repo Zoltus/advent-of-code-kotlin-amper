9370ebc691baf9e23d48019ec6502cce1469fe97
Zoltus <zoltus@outlook.com>
2024-12-04 21:47:22 +0200

    feat: complete day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,31 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    //Part1
+    val pairs: List<Pair<Int, Int>> = input.map { line ->
+        val left = line.substringBefore(" ")
+        val right = line.substringAfterLast(" ")
+        left.toInt() to right.toInt()
+    }
+
+    val left = pairs.map { it.first }.sorted()
+    val right = pairs.map { it.second }.sorted()
+
+    val distances: List<Int> = left.mapIndexed { i, leftVal ->
+        val matching = right[i]
+        val distance = abs(leftVal - matching)
+        return@mapIndexed distance
+    }
+
+    val sumDistances = distances.sum()
+    println("Sum $sumDistances")
+
+    //Part 2
+    val similarityScore = left.sumOf { lNum ->
+        val count = right.count { rNum -> rNum == lNum }
+        lNum * count
+    }
+    println("SimilarityScore $similarityScore")
 }
