dea3d674500fe772ad83d9c3a27724dcf6818a30
Zoltus <zoltus@outlook.com>
2024-12-05 11:35:17 +0200

    feat: complete day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,37 +2,45 @@ import kotlin.math.abs
 
 fun main() {
     val input: List<String> = readInput("Day02")
+    val reports: List<List<Int>> = input.map { row -> row.split(" ").map { it.toInt() } }
     //Part1
-    val safeReportCount: Int = input.count { report ->
-        val levels = report.split(" ").map { it.toInt() }
-        // Gets each possible pair next to each other
-        val zipWithNext: List<Pair<Int, Int>> = levels.zipWithNext()
-        //Check if it should be increasing/degreasing
-        val increasing = zipWithNext[0].first < zipWithNext[0].second
-        val isSafe: Boolean = zipWithNext.all { (left, right) ->
-            // Get dif
-            val dif = abs(left - right)
-            val isUnsafeDif = dif > 3 || dif < 1
-            if (increasing) {
-                left < right && !isUnsafeDif
-            } else {
-                right < left && !isUnsafeDif
-            }
-        }
-        isSafe
-    }
+    val safeReportCount = reports.count(::isSafe)
     println("Safe report count $safeReportCount")
-
     //Part2
+    val safeReportCount2: Int = reports.count { report ->
+        // if safe dont force
+        if (isSafe(report)) {
+            true
+        } else {
+            //Force check with 1 tolerance
+            for (i in 0..<report.size) {
+                val mutList = report.toMutableList()
+                mutList.removeAt(i)
+                if (isSafe(mutList)) {
+                    return@count true
+                }
+            }
+            false
+        }
+    }
+    println("Safe report count2 $safeReportCount2")
+
 }
 
-/*
-Line = report
-num = level
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
- */
+fun isSafe(levels: List<Int>): Boolean {
+    // Gets each possible pair next to each other
+    val zipWithNext: List<Pair<Int, Int>> = levels.zipWithNext()
+    //Check if it should be increasing/degreasing
+    val increasing = zipWithNext[0].first < zipWithNext[0].second
+    val isSafe: Boolean = zipWithNext.all { (left, right) ->
+        // Get dif
+        val dif = abs(left - right)
+        val isUnsafeDif = dif > 3 || dif < 1
+        if (increasing) {
+            left < right && !isUnsafeDif
+        } else {
+            right < left && !isUnsafeDif
+        }
+    }
+    return isSafe
+}
